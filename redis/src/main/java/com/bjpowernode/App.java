03d23296f35ce9b9014797a1c5e4ad413d65f5c8
cjc <bluecats2060@163.com>
2021-07-02 20:07:37 +0800

    的规划

--- redis/src/main/java/com/bjpowernode/App.java
+++ redis/src/main/java/com/bjpowernode/App.java
@@ -6,6 +6,7 @@ public class App
 {
     public static void main( String[] args )
     {
+		//我是注释
         Jedis jedis = new Jedis("127.0.0.1",6379);
         jedis.flushAll();//清空redis中数据
         jedis.set("username","tom");
